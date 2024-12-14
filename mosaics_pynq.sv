941008cf11d531883f8b5df4c52b1c72e6217018
lcambl <97252821+lcambl@users.noreply.github.com>
2024-12-13 18:34:47 -0800

    Update mosaics_pynq.sv

--- mosaics_pynq.sv
+++ mosaics_pynq.sv
@@ -10,39 +10,3 @@ module mosaics_pynq(input logic CLK_IN,
 	pipeline #(ROI_DEPTH, ROI_WIDTH, POI_DEPTH, POI_WIDTH) pl (.clk(CLK_IN), .reset(sws_2bits_tri_i[0]), .status(leds_4bits_tri_o[0]));
 						  
 endmodule 
-
-module mosaics_pynq_tb;
-
-	logic CLK_IN;
-	logic [1:0] sws_2bits_tri_i;
-	logic [3:0] leds_4bits_tri_o;
-
-	// Instantiate the module under test (MUT)
-	mosaics_pynq dut (
-	 .CLK_IN(CLK_IN),
-	 .sws_2bits_tri_i(sws_2bits_tri_i),
-	 .leds_4bits_tri_o(leds_4bits_tri_o)
-	);	
-	
-	
-//	 always begin
-//        #0.5 CLK_IN = ~CLK_IN;  // Toggle every 0.5 ns for a 1 GHz clock (1 ns period)
-//    end
-	initial begin
-        CLK_IN = 0; // Initialize clock to 0
-        forever #4 CLK_IN = ~CLK_IN; // Toggle clock every 4 ns
-    end
-    initial begin
-		  sws_2bits_tri_i[0] <= 1; @(posedge CLK_IN);
-		                           @(posedge CLK_IN);
-		                           @(posedge CLK_IN);
-		                           @(posedge CLK_IN);
-		                           @(posedge CLK_IN);
-		                           @(posedge CLK_IN);
-		  sws_2bits_tri_i[0] <= 0; @(posedge CLK_IN);
-		                           @(posedge CLK_IN);
-		  #1000000;
-        $finish;
-    end
-
-endmodule 
